f2408c01a0a72f95e33751050b0d666b35a721d0
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2025-11-05 23:41:25 +0100

    improve line creation

--- mode/linelike.c
+++ mode/linelike.c
@@ -8,7 +8,7 @@ bool linelike__on_mouse_down(AppState *as, Pos2D const *w_mouse_pos,
                              LineExtMode ext_mode) {
   LinelikeModeData *data = &as->es.data.linelike;
 
-  if (data->saved != NULL) {
+  if (data->saved == NULL) {
     PointDef pot;
     PointDef *pd = get_potential_point(as, w_mouse_pos, &pot);
     if (pd == NULL)
@@ -16,17 +16,25 @@ bool linelike__on_mouse_down(AppState *as, Pos2D const *w_mouse_pos,
     if (pd == NULL)
       return false;
 
-    LineDef *ld = alloc_and_reg_line(
-        &as->gs, make_line_point_to_point(ext_mode, data->saved, pd));
-    if (ld == NULL)
-      return false;
-    data->saved = NULL;
-  } else {
-    PointDef *hovered = get_hovered_point(as, w_mouse_pos);
-    if (hovered == NULL)
-      return true;
-    data->saved = hovered;
+    data->saved = pd;
+
+    return true;
   }
+
+  PointDef pot;
+  PointDef *pd = get_potential_point(as, w_mouse_pos, &pot);
+  if (pd == NULL)
+    pd = alloc_and_reg_point(&as->gs, pot);
+  if (pd == NULL)
+    return false;
+
+  LineDef *ld = alloc_and_reg_line(
+      &as->gs, make_line_point_to_point(ext_mode, data->saved, pd));
+  if (ld == NULL)
+    return false;
+
+  data->saved = NULL;
+
   return true;
 }
 
@@ -34,25 +42,29 @@ bool linelike__on_render(AppState *as, Pos2D const *w_mouse_pos,
                          LineExtMode ext_mode) {
   LinelikeModeData *data = &as->es.data.linelike;
 
-  if (data->saved == NULL)
+  PointDef pot;
+  PointDef *pd = get_potential_point(as, w_mouse_pos, &pot);
+
+  if (data->saved != NULL) {
+    LineDef cursor_line = make_line_point_to_point(ext_mode, data->saved, &pot);
+    draw_line(as, &cursor_line, CYAN);
+  }
+
+  // don't redraw already existing point
+  if (pd != NULL)
     return true;
 
-  PointDef pot;
-  get_potential_point(as, w_mouse_pos, &pot);
   draw_point(as, &pot, CYAN);
-  LineDef cursor_line = make_line_point_to_point(ext_mode, data->saved, &pot);
-
-  draw_line(as, &cursor_line, CYAN);
 
   return true;
 }
 
 bool segment__on_mouse_down(AppState *as, Pos2D const *w_mouse_pos) {
-  return linelike__on_mouse_down(as, w_mouse_pos, L_EXT_LINE);
+  return linelike__on_mouse_down(as, w_mouse_pos, L_EXT_SEGMENT);
 }
 
 bool line__on_mouse_down(AppState *as, Pos2D const *w_mouse_pos) {
-  return linelike__on_mouse_down(as, w_mouse_pos, L_EXT_SEGMENT);
+  return linelike__on_mouse_down(as, w_mouse_pos, L_EXT_LINE);
 }
 
 bool ray__on_mouse_down(AppState *as, Pos2D const *w_mouse_pos) {
